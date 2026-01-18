86d8ceee7caee0a1974af484f8892606d3f583bb
shahip2016 <shahip2016@gmail.com>
2026-01-18 18:03:03 +0530

    Add training script

---
+++ train.py
@@ -0,0 +1,64 @@
+import os
+import torch
+import torch.nn as nn
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+from torch import optim
+from utils import *
+from modules import UNet
+import logging
+from torch.utils.tensorboard import SummaryWriter
+from diffusion import Diffusion
+
+logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=logging.INFO, datefmt="%I:%M:%S")
+
+
+def train(args):
+    setup_logging(args.run_name)
+    device = args.device
+    dataloader = get_data(args)
+    model = UNet().to(device)
+    optimizer = optim.AdamW(model.parameters(), lr=args.lr)
+    mse = nn.MSELoss()
+    diffusion = Diffusion(img_size=args.image_size, device=device)
+    logger = SummaryWriter(os.path.join("runs", args.run_name))
+    l = len(dataloader)
+
+    for epoch in range(args.epochs):
+        logging.info(f"Starting epoch {epoch}:")
+        pbar = tqdm(dataloader)
+        for i, (images, _) in enumerate(pbar):
+            images = images.to(device)
+            t = diffusion.sample_timesteps(images.shape[0]).to(device)
+            x_t, noise = diffusion.noise_images(images, t)
+            predicted_noise = model(x_t, t)
+            loss = mse(noise, predicted_noise)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            pbar.set_postfix(MSE=loss.item())
+            logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i)
+
+        sampled_images = diffusion.sample(model, n=images.shape[0])
+        save_images(sampled_images, os.path.join("results", args.run_name, f"{epoch}.jpg"))
+        torch.save(model.state_dict(), os.path.join("models", args.run_name, f"ckpt.pt"))
+
+
+def launch():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--run_name', type=str, default="DDPM_Uncondtional")
+    parser.add_argument('--epochs', type=int, default=500)
+    parser.add_argument('--batch_size', type=int, default=12)
+    parser.add_argument('--image_size', type=int, default=64)
+    parser.add_argument('--dataset_path', type=str, default="data")
+    parser.add_argument('--device', type=str, default="cuda" if torch.cuda.is_available() else "cpu") # Use CUDA if available, else CPU
+    parser.add_argument('--lr', type=float, default=3e-4)
+    args = parser.parse_args()
+    train(args)
+
+
+if __name__ == '__main__':
+    launch()
